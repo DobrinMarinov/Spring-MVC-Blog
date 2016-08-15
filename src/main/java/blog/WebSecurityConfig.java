8fb0634888dadd83f3ae59152d6935d572bfa71f
Dobrin Marinov <dobrin.marrinov@gmail.com>
2016-08-15 20:00:31 +0300

    Changes made in css styles, png and WebSecurityConfig

--- src/main/java/blog/WebSecurityConfig.java
+++ src/main/java/blog/WebSecurityConfig.java
@@ -1,14 +1,18 @@
 package blog;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+import javax.sql.DataSource;
+
 @Configuration
 @EnableWebSecurity
+@ComponentScan(value = "org.webapp")
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -25,10 +29,18 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .permitAll();
     }
 
+//    @Autowired
+//    private DataSource dataSource;
+
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
         auth
-                . inMemoryAuthentication()
+                .inMemoryAuthentication()
                 .withUser("user").password("password").roles("USER");
+
+//                .jdbcAuthentication()
+//                    .dataSource(restDataSourse)
+//                .withDefaultSchema()
+//                .withUser("user").password("password").roles("USER");
     }
 }
