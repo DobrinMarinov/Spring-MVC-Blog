d9d3a32c7f4f4a22adbae778497f131e2c559473
Dobrin Marinov <dobrin.marrinov@gmail.com>
2016-08-18 00:15:18 +0300

    Users page added

---
+++ src/main/java/blog/controllers/UsersController.java
@@ -0,0 +1,54 @@
+package blog.controllers;
+
+import blog.models.Post;
+import blog.models.User;
+import blog.services.NotificationService;
+import blog.services.PostService;
+import blog.services.UserService;
+import javafx.geometry.Pos;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Controller
+public class UsersController {
+
+    @Autowired
+    private PostService postService;
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private NotificationService notificationService;
+
+    @RequestMapping("/users")
+    public String home(Model model) {
+        List<User> allUsers = userService.findAll();
+        model.addAttribute("allUsers", allUsers);
+
+        return "users";
+
+    }
+
+    @RequestMapping("/users/view/{id}")
+    public String view(@PathVariable("id") Long id, Model model) {
+        User user = userService.findById(id);
+
+        Set<Post> allPosts = user.getPosts();
+
+        if (user == null) {
+            notificationService.addErrorMessage("Cannot find user: " + id);
+            return "redirect:/";
+        }
+
+        model.addAttribute("user", allPosts);
+        return "posts/allUserPosts";
+    }
+}
